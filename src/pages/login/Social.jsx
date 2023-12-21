25332c2b4bd941fc185c457f15a6cd723ddafbed
Adib Khan <110726624+adibkhandev@users.noreply.github.com>
2023-12-21 09:44:30 -0800

    lesgo

--- src/pages/login/Social.jsx
+++ src/pages/login/Social.jsx
@@ -50,6 +50,7 @@ export const Social = () => {
                     </div>
                 </LoginSocialGoogle>
                 <LoginSocialLinkedin
+                    scope = 'r_basicprofile'
                     redirect_uri='https://iridescent-dusk-085b1e.netlify.app/login'
                     onLoginStart={()=>{
 
