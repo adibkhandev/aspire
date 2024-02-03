16339621586fda87cbbb72fcbb6cde80d037ad17
adibkhandev <adibkhaan001@gmail.com>
2024-02-03 13:01:30 +0600

    popup logic

--- src/pages/components/FileImage.jsx
+++ src/pages/components/FileImage.jsx
@@ -4,24 +4,61 @@ import Cam from '../components/svg/Cam';
 const Image = ({file,input,existing}) => {
     const [updated,setUpdated] = useState(false)
     let [url,setUrl] = useState(null)
-    useEffect(() => {
-        if(file){
-            const reader = new FileReader()
-            reader.onloadend = () =>{
-                setUrl(reader.result)
-            }
-            reader.readAsDataURL(file)
-        }
-        if(existing && existing.pfp){
-            setUrl( import.meta.env.VITE_API_URL + existing.pfp)
-        }
-        else{
-            setUrl(null)
-        }
-    }, [file,existing])
+    const [changerModel,setChangerModel]=useState(false)
+    const [detached,setDetached]=useState(true)
     useEffect(()=>{
-        console.log('update -' , updated)
-    },[updated])
+       
+    },[file,existing])
+    useEffect(() => {
+        if(file && !detached){
+        }
+
+    }, [file,detached])
+
+
+
+
+
+
+
+
+
+
+
+
+
+    useEffect(()=>{
+        if(detached){
+            if(url){
+                setTimeout(()=>{
+                   setUrl(null)
+                },1000)
+                setChangerModel(false)
+            }
+            else{
+                if(file){
+                    setDetached(false)
+                    const reader = new FileReader()
+                    reader.onloadend = () =>{
+                        setUrl(reader.result)
+                    }
+                    reader.readAsDataURL(file)
+                }
+                if(existing){
+                    setDetached(false)
+                    setUrl(import.meta.env.VITE_API_URL + existing.pfp)
+                }
+            }
+        }
+    },[detached,file])
+    useEffect(()=>{
+        console.log('update -' , file)
+    },[file])
+    // useEffect(()=>{
+    //     if(!url){
+    //         setDetached(false)
+    //     }
+    // },[url])
          return(
             <>
             
@@ -29,34 +66,47 @@ const Image = ({file,input,existing}) => {
              whileTap={{ scale: 0.9 }}
              onClick={()=>{
                console.log(input.current)
+
                if(input){
+               if(url){
+                   setChangerModel(true)
+                }
+                if(!url && !changerModel){
                    input.current.click()
-                   setTimeout(()=>{
-                      setUrl(null)
-                   },2000)
-                  } 
+                }
+              } 
              }}
                className="pfp-clicker">
 
 
-
-                {url || (existing && existing.pfp) ?(
+ 
                     <div className="mask">
                         <motion.div 
                         className={`pfpUploadContainer`}
                         initial={{ scale: 0}}
-                        animate={url?{ scale: 1 }:{scale:0}}
+                        animate={!detached ?{ scale: 1 }:{scale:0}}
                         transition={{ ease: "linear", duration: 6 ,type:'spring', mass: 0.8 ,stiffness:80 }}
-                        // onAnimationComplete={()=> {
-                        //     setUpdated(true)
-                        //     console.log('changing')
-                        // }}
                         >
-                            <img className='pfpUpload' src={url?url:import.meta.env.VITE_API_URL + existing.pfp} alt=""/>
+                            <img className='pfpUpload' src={url} alt=""/>
                         </motion.div>
-
                     </div>
-                ):''}
+                        <motion.div 
+                          className="options"
+                          initial={{scale:0}}
+                          animate={changerModel?{scale:1}:{scale:0}}
+                        >
+                            <div onClick={()=>{
+                                if(input){
+                                    input.current.click()
+                                    setDetached(true)
+                                }
+                            }} id="first" className="option">Change picture</div>
+                            <div
+                             onClick={()=>{
+                                setDetached(true)
+                             }} 
+                             className="option">Remove upload</div>
+                        </motion.div>
              
              
              <motion.div
