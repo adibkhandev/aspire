9cea508d3ae6b7c7acfc9859cca848909de4b177
adibkhandev <adibkhaan001@gmail.com>
2024-02-18 03:21:51 +0600

    subscribe animated

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -13,6 +13,20 @@ const Player = ({course,setHeight,popupRef}) => {
     const containerRef = useRef(null)
     const navigate = useNavigate()
     const token = localStorage.getItem('accessToken');
+    const [user,setUser] = useState(localStorage.getItem('userData')?JSON.parse(localStorage.getItem('userData')):null)
+    const [subscribed,setSubscribed] = useState(true)
+    useEffect(()=>{
+        console.log(user,'asdasdmeeeeeeeeeeeeee')
+        if(user && course) {
+            if(user.subscribedCourses.includes(course._id)){
+                setSubscribed(true)
+            }
+            else{
+                setSubscribed(false)
+            }
+        }
+    },[user,course])
+    console.log(user,'usreerere')
     useLayoutEffect(() => {
         // console.log(popupRef.current.clientHeight)
         if(popupRef){
@@ -22,24 +36,28 @@ const Player = ({course,setHeight,popupRef}) => {
     });
     
 
-    const subscribe = () => {
-        console.log('clic')
+    const subscribe = (process) => {
+        console.log(process,'process')
         const data = {
             courseId:course._id
         }
         const headers = {
             headers:{
-              'Authorization':'Bearer ' + token,
-              'Content-Type':'application/json',
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+                'Authorization':'Bearer ' + token,
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
               'Access-Control-Request-Method': 'GET, POST, DELETE, PUT, OPTIONS'
             }
           }
-        const url =  import.meta.env.VITE_API_URL + '/user/subscribe' 
-        axios.post(url,data,headers)
-           .then((response)=>{
-              console.log(response.data,'ssubbss')
+          const url =  import.meta.env.VITE_API_URL +  `/user/${process=='un'?'un':''}subscribe` 
+          axios.post(url,data,headers)
+          .then((response)=>{
+              console.log(response.data.user,'ssubbss')
+              setUser(response.data.user)
+              localStorage.setItem('userData',JSON.stringify(response.data.user))
+              if(process=='in') setSubscribed(true)
+              else setSubscribed(false)
            })
            .catch((err)=>{
               console.log(err.response.status)
@@ -93,14 +111,26 @@ const Player = ({course,setHeight,popupRef}) => {
                    {activeVideo?activeVideo.description:course.description}
                 </h1>
                 <motion.div
-                  onClick={()=>subscribe()} 
+                  onClick={()=>{
+                      if(subscribed){
+                          subscribe('un')
+                      }
+                      else{
+                        subscribe('in')
+                      }
+                  }} 
                   className="subscription">
-                    <svg  viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M0.5 5.5C0.5 2.73877 2.73877 0.5 5.5 0.5C8.26123 0.5 10.5 2.73877 10.5 5.5C10.5 8.26123 8.26123 10.5 5.5 10.5C2.73877 10.5 0.5 8.26123 0.5 5.5Z" stroke="#525252"/>
-                      <path d="M4.93801 7.74989L2.95801 5.76989L3.75338 4.97452L4.93801 6.15914L7.58738 3.50977L8.38276 4.30514L4.93801 7.74989Z" fill="#545454"/>
+                    <svg   viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <motion.path  
+                        d="M0.5 5.5C0.5 2.73877 2.73877 0.5 5.5 0.5C8.26123 0.5 10.5 2.73877 10.5 5.5C10.5 8.26123 8.26123 10.5 5.5 10.5C2.73877 10.5 0.5 8.26123 0.5 5.5Z" 
+                        initial={{fill:'none',stroke:"#525252"}}
+                        animate={subscribed?{fill:"#0085FF",stroke:"#0085FF"}:{fill:'rgba(0,0,0,0)',stroke:"#525252"}}
+                        />
+                      <motion.path  d="M4.93801 7.74989L2.95801 5.76989L3.75338 4.97452L4.93801 6.15914L7.58738 3.50977L8.38276 4.30514L4.93801 7.74989Z" 
+                        fill={subscribed?"#D7D7D7":"#545454"}/>
                     </svg>
                     <h1>
-                        Subscribe
+                        {subscribed?'Subscribed':'Subscribe'}
                     </h1>
                 </motion.div>
         </div>
