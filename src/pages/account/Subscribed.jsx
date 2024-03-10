441fde7e3f81d6e861fd0ffb3be3ab9a00969bb6
adibkhandev <adibkhaan001@gmail.com>
2024-03-11 00:41:12 +0600

    responsive fix

--- src/pages/account/Subscribed.jsx
+++ src/pages/account/Subscribed.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect , useState } from 'react'
 import { CoursePack } from '../explore/HorizontalSwiper'
 import {Nav} from './../Nav'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 import linkTo from './../../assets/images/link-to.svg'
 import smallPlay from './../../assets/images/small-play.svg'
@@ -68,17 +69,23 @@ const Card = ({subscribed}) => {
            <div className="card-cont">
                <div className="desc-cont">
                     <div className="text">
+                       
                         <h1 className='title'>
+                          <Link to={`/course/${cardData.courseId}`} >
                             {cardData.courseName}
+                          </Link>                       
                         </h1>
                         <SubscribeCta courseId={cardData.courseId} subscribed={subscribedState} setSubscribed={setSubscribedState} ></SubscribeCta>
                     </div>
-                    <img src={linkTo} alt="" />
+                    <Link to={`/course/${cardData.courseId}`} >
+                      <img className='link' src={linkTo} alt="" />
+                    </Link>
                </div>
                <div className="cover">
                    <img src={import.meta.env.VITE_API_URL + cardData.cover} alt="" />
                </div>
                <div className="end">
+                 <Link to={`/profile/${cardData.uploaderName}`}>
                    <div className="author-cont">
                         <div className="pfp-cont">
                             <img src={import.meta.env.VITE_API_URL + cardData.uploaderPicture} alt="" />
@@ -86,7 +93,9 @@ const Card = ({subscribed}) => {
                         <div className="author-name">
                             {cardData.uploaderName}
                         </div>
+                       
                     </div>
+                  </Link>
                     <div className="videoCount">
                             <div className="num">
                             {cardData.videos}
