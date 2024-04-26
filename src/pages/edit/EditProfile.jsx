159ebb3cc42a187a881783341c923e0ccf1f3995
adibkhandev <adibkhaan001@gmail.com>
2024-04-27 01:49:57 +0600

    CHANGE PASS

--- src/pages/edit/EditProfile.jsx
+++ src/pages/edit/EditProfile.jsx
@@ -109,62 +109,147 @@ const EditProfile = () => {
 
 
     ////
+  const [mode,setMode] = useState('edit')
   return (
-    <div className="login-container edit">
+    <div className="edit">
     <Nav></Nav>
-    {user && skills?(
-      <form
-        onClick={(e)=>{
-          e.stopPropagation()
-          if(popupthere){
-            setPopupthere(false)   
-          }
-        }}  
-        onSubmit={(e)=>updateHandler(e)} className='signup'>
-          <Image setRemovePfp={setRemovePfp} popupthere={popupthere} setPopupthere={setPopupthere} existing={user.pfp} file={imageFile} input={clickRef}></Image>
-          <input className='hidden' onChange={(e)=>imageHandler(e)} ref={clickRef} type="file" name="profile" id="" />
-          <div className="data">
-              <div className="title">
-                  Skills
+    <motion.div animate={mode=='auth'?{translateX:'-100vw'}:{translateX:0}} className="form-parent">
+      {user && skills?(
+          <form
+            onClick={(e)=>{
+              e.stopPropagation()
+              if(popupthere){
+                setPopupthere(false)   
+              }
+            }}  
+            onSubmit={(e)=>updateHandler(e)} className='signup'>
+              <Image setRemovePfp={setRemovePfp} popupthere={popupthere} setPopupthere={setPopupthere} existing={user.pfp} file={imageFile} input={clickRef}></Image>
+              <input className='hidden' onChange={(e)=>imageHandler(e)} ref={clickRef} type="file" name="profile" id="" />
+              <div className="data">
+                  <div className="title">
+                      Skills
+                  </div>
+              <BoxAddon skills={skills} setSkills={setSkills} setError={setError} num={5}></BoxAddon>
+              <div className="lock-cont">
+                <div className="icon-cont">
+                  <img onClick={()=>{
+                    console.log('casa')
+                    usernameRef.current.focus()
+                  }} src={squareReverse} alt="" />
+                </div>
+                <input 
+                onChange={(e)=>{
+                  if(e.target.value) setChangesMade(true)
+                  else setChangesMade(false)
+                }} autoComplete='off' ref={usernameRef} name='username' type="text" className="lock" id='name-input' placeholder={user?user.username:''} />
               </div>
-          <BoxAddon skills={skills} setSkills={setSkills} setError={setError} num={5}></BoxAddon>
-          <div className="lock-cont">
-            <div className="icon-cont">
-              <img onClick={()=>{
-                 console.log('casa')
-                 usernameRef.current.focus()
-              }} src={squareReverse} alt="" />
-            </div>
-            <input 
-             onChange={(e)=>{
-              if(e.target.value) setChangesMade(true)
-              else setChangesMade(false)
-            }} autoComplete='off' ref={usernameRef} name='username' type="text" className="lock" id='name-input' placeholder={user?user.username:''} />
-          </div>
 
-          </div>
-          <div className="handy-btns">
-                <motion.div onClick={()=>{
-                    navigate('/')   
-                }}  whileTap={{ scale: 0.98 }}  className="secondary-btn">Discard</motion.div>
-                <MotionCta submit={true} changesMade={changesMade} ></MotionCta>
-          </div>
-          <div className="linking">
-                Wanna change
-                &#13;
-                <span onClick={()=>setMode('signin')} className='underline-line'>
-                    password?
-                </span>
-          </div>
+              </div>
+              <div className="handy-btns">
+                    <motion.div onClick={()=>{
+                        navigate('/')   
+                    }}  whileTap={{ scale: 0.98 }}  className="secondary-btn">Discard</motion.div>
+                    <MotionCta submit={true} changesMade={changesMade} ></MotionCta>
+              </div>
+              <div className="linking">
+                    Wanna change
+                    &#13;
+                    <span onClick={()=>setMode('auth')} className='underline-line'>
+                        password?
+                    </span>
+              </div>
+                  
+              </form>
               
-          </form>
-       
-    ):''} 
+      ):''} 
+      <ChangePasswordForm tokenize={tokenize} setMode={setMode} setError={setError} ></ChangePasswordForm>
+    </motion.div>
+
 
 
        <CustomAlert error={error} setError={setError}/>
     </div>
   )
 }
+
+const ChangePasswordForm = ({setMode,setError,tokenize}) => {
+  const formRef = useRef()
+  const navigate = useNavigate()
+  const [changesMade,setChangesMade] = useState(false)
+  const trackChanges = (e) => {
+     console.log(formRef.current.lastPassword,'e')
+     if(formRef.current.lastPassword.value && formRef.current.lastPasswordConfirmed.value && formRef.current.newPassword.value){
+        setChangesMade(true)
+     }
+     else{
+      setChangesMade(false)
+     }
+  }
+
+  const passwordHandler = (e) => {
+    e.preventDefault()
+    const token = localStorage.getItem('accessToken')
+    console.log('pass',e.target.lastPassword.value)
+    if(e.target.lastPassword.value!=e.target.lastPasswordConfirmed.value){
+        setError("Passwords don't match")
+        return
+    }
+    const data = {
+       lastPassword:e.target.lastPassword.value,
+       lastPasswordConfirmed:e.target.lastPasswordConfirmed.value,
+       newPassword:e.target.newPassword.value
+    }
+    const headers = {
+      headers:{
+        'Authorization':`Bearer ${token}`,
+        'Content-Type':'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+      }
+    }
+    console.log(data,'dd')
+    const url =  import.meta.env.VITE_API_URL + '/auth/password/update' 
+    axios.post(url,data,headers)
+              .then((response)=>{
+                      console.log(response.data,'data')
+                      tokenize(response.data.accesstoken,response.data.refreshtoken,response.data.user)
+                      navigate('/') 
+              })
+              .catch((err)=>{
+                console.log(err);
+                // setError(err.response.data.message)
+              })
+  }
+  
+  return(
+    <form 
+    ref={formRef}
+    onChange={(e)=>trackChanges(e)}
+    onSubmit={(e)=>{
+        passwordHandler(e)
+    }}
+    className="change w-screen">
+      <div className="inputs">
+        <input type="text" name="lastPassword" placeholder='Type your current password' className="regular-inputs" />
+        <input type="text" name='lastPasswordConfirmed' placeholder='Type the password again' className="regular-inputs" />
+        <input type="text" name='newPassword' placeholder='New password ?' className="regular-inputs" />
+        <div className="handy-btns">
+                <motion.div onClick={()=>{
+                    navigate('/')   
+                }}  whileTap={{ scale: 0.98 }}  className="secondary-btn">Discard</motion.div>
+                <MotionCta submit={true} changesMade={changesMade} ></MotionCta>
+          </div>
+          <div className="linking">
+                <span onClick={()=>setMode('edit')} className='underline-line'>
+                    {'<Edit my profile'}
+                </span>
+                &#13;
+                instead
+          </div>
+      </div>
+  </form>
+   )
+}
+
 
 export default EditProfile
