1dc8767a0dffb4078d4aaad94d0ec54220e7aa3a
adibkhandev <adibkhaan001@gmail.com>
2024-05-17 02:14:13 +0600

    LESSGO

--- src/pages/components/BoxAddon.jsx
+++ src/pages/components/BoxAddon.jsx
@@ -8,30 +8,41 @@ import { StyledEngineProvider } from '@mui/material/styles';
 export const BoxAddon = ({skills,num,setError,setSkills,defaultSkills}) => {
 const list = ['Physics','Chemistry','Hoe','Music','Dance','Acting','Art','Finance']
 let [open,setOpen] = useState(true)
-const [inLimit,setInLimit]=useState(true);    
+const [outtaLimit,setOuttaLimit]=useState(()=>{ return false
+});    
+useEffect(()=>{
+   console.log(outtaLimit,'limit')
+},[outtaLimit])
 let inputRef = useRef(null)
+let muiInputRef = useRef(null)
 return (
      <StyledEngineProvider injectFirst>
         <div className="textarea-container">
          <Autocomplete
            multiple
-           id="multiple-limit-tags "
+           id="multiple-limit-tags"
            options={list}
            value={skills}
+           blurOnSelect={true}
            PopperComponent={(props) => (
-             <Popper style={{marginTop:'1000px'}} id='popper' {...props} />
+             <Popper  style={{marginTop:'1000px'}} id='popper' {...props} />
            )}
            getOptionLabel={(option) => option}
            onChange={(e,value)=>{
              console.log(value,'value')
              if(value.length>6){
-                 setInLimit(false)
+                 setOuttaLimit(true)
                  setError('Max number of interests reached')
              }
-             else{
+             if(value<1){
+              setOuttaLimit(true)
+              setError('Minimum number of interests reached')
+             }
+             if(6<value<1){
                  setSkills(value)
              }
            }}
+           
            renderTags={(tagValue, getTagProps) =>
              tagValue.map((option, index) => (
                <Chip
@@ -42,11 +53,15 @@ return (
                   {...getTagProps({ index })}
                />
             ))}
+            ref={muiInputRef}
             renderInput={(params) => (
-                <TextField ref={inputRef} sx={{outline:'none',padding:'0'}} placeholder='Search for skills'{...params} disabled={!inLimit} />
+              <TextField ref={inputRef} sx={{outline:'none',padding:'0'}} placeholder='Search for skills'{...params}  />
             )}
-            sx={{ width: '500px' }}
-            disabled={!inLimit}
+            sx={
+              {
+                width: '500px'
+              }
+            }
         />
        </div>
      </StyledEngineProvider>
