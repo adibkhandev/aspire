74f123e2d52b58de7b620f47c457554fb8bdb3e8
adibkhandev <adibkhaan001@gmail.com>
2024-05-20 18:34:49 +0600

    LOADERKILLER

--- src/pages/login/Loader.jsx
+++ src/pages/login/Loader.jsx
@@ -2,23 +2,26 @@ import React , {useState} from 'react'
 import { motion, useMotionValue, useSpring, useTransform } from 'framer-motion'
 import { useEffect } from 'react'
 export const Loader = ({children,userType,setUserType}) => {
-    const value = 30
+    const escapeTime = 0.1
     return (
         <>
             <div className="loader-cont">
               <motion.div 
-                // animate={value?{height:`${value}%`}:{height:auto}} 
+                initial={{height:'20%'}}
+                animate={!userType?{height:['20%','75%','20%']}:{y:'-150vh'}} 
+                transition={!userType?{duration:6,repeat:Infinity,repeatType:'loop'}:{duration:escapeTime,delay:0.6}}
                 onClick={()=>{
                     setUserType('teacher')
                 }
             }  className={userType?"teacher-cont topSlide mover":"teacher-cont mover"}>
                     <div className="heading">
-                        <h1 className={userType?'goLeft':''}>
+                        <motion.h1
+                         animate={userType?{x:'-100vw'}:{x:0}}>
                             Choose 
-                        </h1>
-                        <h1 className={userType?'goRight':''}>
+                        </motion.h1>
+                        <motion.h1 animate={userType?{x:'100vw'}:{x:0}}>
                             your mode
-                        </h1>
+                        </motion.h1>
                     </div>
                     <div className={`tap-info ${!userType?'pulse':'vanish'}`}>
                        Tap here if you are a teacher
@@ -32,11 +35,13 @@ export const Loader = ({children,userType,setUserType}) => {
 
                 <motion.div 
                 //    animate={value?{height:`${100-value}%`}:{height:auto}} 
+                animate={!userType?{y:0}:{y:'100vh'}}
+                transition={{duration:escapeTime,delay:0.6}}
                   onClick={()=>{
                      setUserType('student')
                    }
                  } 
-                 className={userType?"student-cont bottomSlide":"student-cont"}>
+                 className={!userType?"student-cont bottomSlide":"student-cont"}>
                     <div 
                        className={`tap-info ${!userType?'pulse':'vanish'}`}>
                        Tap here if you are a student
