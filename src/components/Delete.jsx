7557915f40ea95897f3a447a6196a8a4623f0fb9
adibkhandev <adibkhaan001@gmail.com>
2025-03-06 00:57:42 +0600

    LOGOUT

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect ,useRef} from 'react'
+import React, { useEffect ,useRef , useState} from 'react'
 import { motion ,animate} from 'framer-motion'
 
 const Delete = ({deleteMode,setDeleteMode,setDeleteInitiated,deletePrompt,setDeletePrompt}) => {
@@ -68,54 +68,79 @@ const Delete = ({deleteMode,setDeleteMode,setDeleteInitiated,deletePrompt,setDel
 
 export const LogoutPopup = ({logout,loggingOut,setLoggingOut}) => {
     const scope = useRef(null)
+    const timeForClose = 0.5
+    const [closing,setClosing] = useState(false)
     const animateDelete = async () => {
-        if(loggingOut){
+        if(loggingOut && !closing){
+            // await animate(scope.current,{
+            //      opacity:1  
+            // },{
+            //     delay:1
+            // })
             await animate(scope.current,{
+                opacity:1,
                 backdropFilter:'brightness(50%) blur(10px)',
                 background:'#2727270c',
                 zIndex:5
                 
+            },{
+                delay:timeForClose
             })
         }
         else{
+            console.log('condition workds')
             await animate(scope.current,{
                 backdropFilter:'brightness(100%) blur(0px)',
                 background:'transparent',
+            },{
+                delay:0.2,
             })
             await animate(scope.current,{
+
                 zIndex:-1
+            },{
+                onComplete: ()=> {
+                    setLoggingOut(false)
+                    setClosing(false)
+                } 
             })
         }
     } 
     useEffect(()=>{
         animateDelete()
-    },[loggingOut])
+    },[loggingOut,closing])
+
+
  if(loggingOut) return (
     <motion.div
+    style={{opacity:0}}
     transition={{delay:0.08}}
     ref={scope} 
     onClick={(e)=>{
         e.stopPropagation()
-        setLoggingOut(false)
+        // setLoggingOut(false)
+        setClosing(true)
     }}
       className='delete-screen'>
           <motion.div
             initial={{y:"100vh"}}
-            animate={loggingOut?{y:0,scale:1}:{y:"100vh",scale:0.4}} 
-            transition={loggingOut?{delay:0.2}:{delay:0}}
+            animate={loggingOut && !closing?{y:0,scale:1}:{y:"100vh",scale:0.4}} 
+            transition={loggingOut && !closing?{delay:timeForClose + 0.2}:{delay:0}}
             className="delete-dialog">
                <div className="text">
                     Are you sure you want to discontinue?
                </div>
                <div className="options">
                    <div onClick={()=>{
-                       setLoggingOut(false)
+                    //    setLoggingOut(false)
+                    setClosing(true)
                        logout()
                     }} className="confirm">
                         Confirm
                    </div>
                    <div onClick={()=>{
-                       setLoggingOut(false)
+                    //    setLoggingOut(false)
+                    setClosing(true)
                     }} className="cancel">
                         Cancel
                    </div>
